d25901129189003656fa241411298b4cd3aed584
petergeelhoed <petergeelhoed@gmail.com>
2026-04-17 14:30:42 +0200

    Add timestamps
    Print iso dates before the log message

--- src/slog.c
+++ src/slog.c
@@ -18,6 +18,8 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/time.h>
+#include <time.h>
 
 #ifndef SLOG_SIZE
 #define SLOG_SIZE 4096
@@ -28,9 +30,34 @@ static char* g_slogpos = g_slog;
 
 static pthread_mutex_t g_slog_mutex = PTHREAD_MUTEX_INITIALIZER;
 
+static void slog_timestamp(char* buf, size_t buflen)
+{
+    struct timeval time_val;
+    struct tm time;
+
+    gettimeofday(&time_val, NULL);
+    localtime_r(&time_val.tv_sec, &time);
+
+    // NOLINTBEGIN(readability-magic-numbers)
+    /* YYYY-MM-DD HH:MM:SS.mmm */
+    (void)snprintf(buf,
+                   buflen,
+                   "%04d-%02d-%02dT%02d:%02d:%02d.%03ld",
+                   time.tm_year + 1900,
+                   time.tm_mon + 1,
+                   time.tm_mday,
+                   time.tm_hour,
+                   time.tm_min,
+                   time.tm_sec,
+                   time_val.tv_usec / 1000);
+    // NOLINTEND(readability-magic-numbers)
+}
+
 void slog(const char* fmt, ...)
 {
     va_list aplist;
+    const int ts_buf_len = 32;
+    char timestamp[ts_buf_len];
 
     pthread_mutex_lock(&g_slog_mutex);
 
@@ -40,6 +67,17 @@ void slog(const char* fmt, ...)
         pthread_mutex_unlock(&g_slog_mutex);
         return;
     }
+
+    slog_timestamp(timestamp, ts_buf_len);
+    int ts_written = snprintf(g_slogpos, remaining, "[%s] ", timestamp);
+    if (ts_written <= 0 || (size_t)ts_written >= remaining)
+    {
+        pthread_mutex_unlock(&g_slog_mutex);
+        return;
+    }
+
+    g_slogpos += ts_written;
+    remaining -= (size_t)ts_written;
 
     va_start(aplist, fmt);
     int written = vsnprintf(g_slogpos, remaining, fmt, aplist);
